6b97aebe9fedbe962f07886c4e61e45b6dd651fa
KKrokhaleva <kkrohaleva93@gmail.com>
2021-12-27 00:15:51 +0200

    added homework9 and homework10

---
+++ Homework10/script.js
@@ -0,0 +1,66 @@
+let obj = {
+	a: 'f',
+	b: 78,
+	c: 'R',
+	d: {
+		a: {
+			a: null,
+			b: 'E',
+			c: {
+				a: true,
+				b: 'C',
+				c: 'test'
+			},
+			d: 'U'
+		},
+		b: {
+			a: 'R',
+			b: ['S', 4, 6, 'I'],
+			c: 0,
+		},
+		c: ['O'],
+		d: null,
+		e: 'N'
+	}
+}
+
+// Функция для проверки строки на верхний регистр
+function chekToUpperCase(item) {
+	if (typeof item === 'string') {
+		if (item === item.toUpperCase()) {
+			return true;
+		}
+	} else {
+		return false;
+	}
+}
+
+let result = "";
+
+// Функция для выборки из объекта строковых значенй в верхнем регистре
+function getUpperCaseLetter(obj) {
+	for (let [index, key] of Object.entries(obj)) {
+		if (typeof key === 'string') {
+			if (chekToUpperCase(key)) {
+				result += key; continue;
+			}
+		}
+		if (Array.isArray(key)) {
+			key.forEach(
+				function (item) {
+					if (chekToUpperCase(item)) {
+						result += item;
+					}
+				}
+			)
+			continue;
+		}
+		if (typeof key === "object" && key) {
+			getUpperCaseLetter(key);
+		}
+	}
+}
+
+
+getUpperCaseLetter(obj);
+alert(result);
